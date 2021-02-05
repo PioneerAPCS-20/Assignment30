9e853a54ae58caf9be7934b4e82e5419934e344c
codytheking <codytheking93@gmail.com>
2021-02-05 10:26:26 -0800

    Starter code

---
+++ Main.java
@@ -0,0 +1,27 @@
+ /**
+ * 
+ * @author 
+ * @version 
+ */
+
+public class Main
+{   
+    public static void main(String[] args) 
+    {
+        final String FILE_ONE = "grid1.txt";
+        final String FILE_TWO = "grid2.txt";
+        final String FILE_THREE = "grid3.txt";
+
+
+        // create object using file names provided above
+        
+        // print hourglass sum and for each of the three files
+
+
+
+
+        // Solutions (should match your ouput above)
+        System.out.println("\n\nSolutions:");
+        System.out.println("grid1.txt: 13\ngrid2.txt: 19\ngrid3.txt: 52");
+    }
+}
